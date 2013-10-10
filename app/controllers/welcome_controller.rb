06d386b4f91e87205d5a4cae71d53bdc0fad27ed
Spencer Eldred <eldredspencer@gmail.com>
2013-10-10 11:25:20 -0700

    practice rails app from GA class

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+
+  def index
+  end
+
+  def another_index
+    @var = "Some value."
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittenz::Application.config.secret_token = 'dc28b6684e9c800f0fd86ed521cdad17d7cb70b679bfce89928214e36af7e1b967cc0e308906ca3dc053c90f3b1c851446c8ba53a5ac13b095f6142689204930'
